8e77733115e2a2174eb618305b59dc8bddb4e524
dlardo <dlardo@riotgames.com>
2015-02-25 14:26:18 -0800

    changed by_three
    you were passing in cube but you wanted to pass in number.  You can call
    functions by name(value)

--- Help Code/25% help.py
+++ Help Code/25% help.py
@@ -1,4 +1,14 @@
 def cube(number):
     return number * number * number
-def by_three(cube):
-    if number /3():
+
+def by_three(number):
+    if (number/3):
+        print "It's divisible by 3!"
+        return cube(number)
+
+def main():
+    user_number = input('Enter a number: ')
+    by_three(user_number)
+
+if __name__ == "__main__":
+    main()
