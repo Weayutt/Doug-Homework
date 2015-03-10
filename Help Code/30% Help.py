2354313b082f737e41527c80b66d91bf6a984812
Weayutt <wyattmurphy1@gmail.com>
2015-03-09 17:56:50 -0700

    30% Help

---
+++ Help Code/30% Help.py
@@ -0,0 +1,7 @@
+def distance_from_zero(arg):
+    return type(arg) == int or type(arg) == float
+    if type == int or float:
+        return abs(-10)
+    else:
+        return "Nope"
+"""It says Oops, try again. Your function seems to fail on input -10 when it returned 'True' instead of '10' and i dont even have a true anywhere in the code"""
